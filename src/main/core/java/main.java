049c069f76d14ca41b92d3b34002f3ead83b9fa2
Lorenzo <lb.benetti@hotmail.com>
2025-06-09 11:51:37 -0300

    Semana 3 - 86a9a2kwr -> Aplicativo para consultar e inserir dados no banco com JDBC.

--- src/main/core/java/main.java
+++ src/main/core/java/main.java
@@ -1,30 +1,56 @@
+import insert.AddProdutos;
+import insert.ConsultaProdutos;
+
 import java.sql.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/empresa";
+        String url = "jdbc:postgresql://localhost:5432/loja";
         String user = "postgres";
         String password = "adm";
+        Scanner teclado = new Scanner(System.in);
+        int opcao;
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             System.out.println("Conexão estabelecida com sucesso!");
+            String sql = "INSERT INTO produtos (prod_nome, prod_preco, prod_categoria, prod_estoque) VALUES (?,?,?,?)";
+            AddProdutos addProd = new AddProdutos();
+            ConsultaProdutos consulta = new ConsultaProdutos();
 
-            String query = "SELECT * FROM usuarios";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)){
+                try{
+                    do{
+                        System.out.println("""
+                        \n-Aplicativo para consulta e cadastro de produtos na loja-
+                        1- Fazer uma consulta sobre os produtos cadastrados.
+                        2- Cadastrar um novo produto.
+                        0- Sair da aplicação.
+                        """);
+                        System.out.println("Digite a opção do que você quer fazer (1-2 ou 0 para sair): ");
+                        opcao = teclado.nextInt();
 
-            try (Statement stmt = conn.createStatement();
-                 ResultSet rs = stmt.executeQuery(query)) {
+                        if(opcao < 0 || opcao > 2){
+                            System.out.println("Você digitou uma opção inválida. Por favor, digite uma opção válida (1-2, ou 0 para sair)!\n");
+                        }
+                        else if(opcao == 1){
+                            consulta.consultar(conn);
+                        }else if (opcao == 2){
+                            addProd.adicionar(stmt);
+                        }else if (opcao == 0){
+                            System.out.println("Até mais!");
+                            conn.close();
+                        }
+                    }while(opcao != 0);
 
-                while (rs.next()) {
-                    System.out.println("ID: " + rs.getInt("usua_id") + ", Nome: " + rs.getString("usua_nome") + ", Email: " + rs.getString("usua_email"));
+                } catch (Exception e) {
+                    throw new InputMismatchException("Você digitou a opção de maneira incorreta: " + e);
                 }
 
-                rs.close();
-                conn.close();
-                stmt.close();
 
             } catch (SQLException e) {
                 System.out.println("Erro ao executar a consulta: " + e.getMessage());
-
             }
 
         } catch (SQLException e) {
